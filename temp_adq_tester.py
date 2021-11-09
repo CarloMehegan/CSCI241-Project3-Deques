feab55c57ebddb563857a6e438cc44153a9bc740
Carlo <CarloMehegan@gmail.com>
2021-11-09 17:55:11 -0500

    temp file for messing with deque implementations outside of unit tests

---
+++ temp_adq_tester.py
@@ -0,0 +1,21 @@
+from Deque_Generator import get_deque
+
+dq = get_deque(1)
+
+print(str(dq))
+
+dq.push_front(1)
+
+print(str(dq))
+
+dq.push_front(0)
+
+print(str(dq))
+
+dq.pop_front()
+
+print(str(dq))
+
+dq.pop_front()
+
+print(str(dq))
