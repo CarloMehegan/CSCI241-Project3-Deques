4963906bbdc372f35de5d8eafdc165a371ba5365
Carlo <CarloMehegan@gmail.com>
2021-11-09 18:47:35 -0500

    added IndexError to Adq if popping an already empty string

--- Array_Deque.py
+++ Array_Deque.py
@@ -81,6 +81,10 @@ class Array_Deque(Deque):
   def pop_front(self):
     # replace pass with your implementation. Do not reduce the capacity.
 
+    #check if array is empty, if it is, raise error
+    if (self.__length == 0):
+      raise IndexError
+
     #pop operations return the value that is removed
     returned = self.__contents[self.__front_index]
     #change the front index of the deque to (front_index + 1) % self.__capacity
@@ -108,6 +112,10 @@ class Array_Deque(Deque):
   
   def pop_back(self):
     # replace pass with your implementation. Do not reduce the capacity.
+
+    #check if array is empty, if it is, raise error
+    if (self.__length == 0):
+      raise IndexError
 
     #pop operations return the value that is removed
     returned = self.__contents[self.__back_index]
--- temp_adq_tester.py
+++ temp_adq_tester.py
@@ -1,10 +1,14 @@
 from Deque_Generator import get_deque
+from Stack import Stack
 
 dq = get_deque(1)
 
 print(str(dq))
 
 dq.push_front(1)
+print(str(dq))
+dq.pop_front()
+
 
 print(str(dq))
 
@@ -12,10 +16,18 @@ dq.push_front(0)
 
 print(str(dq))
 
-dq.pop_front()
-
 print(str(dq))
 
-dq.pop_front()
+print("stack")
 
-print(str(dq))
+s = Stack()
+print(s)
+s.push(1)
+s.pop()
+# s.pop()
+s.push(2)
+s.push(3)
+print(s)
+popped = s.pop()
+print(s)
+print("popped " + str(popped))
