f579300292c994450fc58f62d48edb5d329ab017
Carlo <CarloMehegan@gmail.com>
2021-11-22 14:22:08 -0500

    changed "top" of a stack to be at the "front" end of the string and vice versa

--- Stack.py
+++ Stack.py
@@ -7,21 +7,21 @@ class Stack:
 
   def __str__(self):
     #string of a stack should look the same as a deque
-    #the "top" of the stack will be at the end of the string
-      #and the "bottom" will be at the start
+    #the "top" of the stack will be at the start of the string
+      #and the "bottom" will be at the end
     return str(self.__dq)
 
   def __len__(self):
     return len(self.__dq)
 
   def push(self, val):
-    self.__dq.push_back(val)
+    self.__dq.push_front(val)
 
   def pop(self):
-    return self.__dq.pop_back()
+    return self.__dq.pop_front()
 
   def peek(self):
-    return self.__dq.peek_back()
+    return self.__dq.peek_front()
 
 # Unit tests make the main section unneccessary.
 #if __name__ == '__main__':
